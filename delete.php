8e0acb1c7d9b6df86687de855a9b3a96cac83ff9
JstGrizz <khalidu234@gmail.com>
2024-08-07 17:18:13 +0700

    Update: Added Alert

--- delete.php
+++ delete.php
@@ -7,6 +7,11 @@ if (isset($_GET['id'])) {
     // Call the function to delete the record
     deleteTreeData($id);
 
+    if (isset($_SESSION['message'])) {
+        echo "<script>alert('" . $_SESSION['message'] . "');</script>";
+        unset($_SESSION['message'], $_SESSION['message_type']);
+    }
+
     // Redirect to the page after deletion
     header("Location: Laporan.php");
     exit();
